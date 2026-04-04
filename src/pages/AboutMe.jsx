d9f042296d225e95e44627022efaa28071d3d020
Paoox <pao.arreola.g@gmail.com>
2026-04-04 17:51:45 -0600

    actualizando AboutMe

--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -50,7 +50,8 @@ function AboutMe() {
           <p id="description-me">
             Soy una apasionada del desarrollo web y de la programación.
             <br />
-            Realice un cambio radical en mi carrera profesional en la cual tengo mas de 20 años de experiencia en el área de ventas, atención a clientes y manejo de personal como Supervisora, Coordinadora y Gerente. <br />
+            Realice un cambio radical en mi carrera profesional en la cual tengo mas de 20 años de experiencia en diferentes areas como ventas, atención a clientes, promotoria, etc.<br />
+            Cuento con apmplia experiencia en manejo de personal como Supervisora, Coordinadora y Gerente. <br />
             Descubri hace unos 5 años atras que me gustaba el tema de la
             programación, <br />
             desde entonces he estado aprendiendo y creciendo en este campo.
